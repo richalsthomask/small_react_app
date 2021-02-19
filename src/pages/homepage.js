09db5e55017cd18b3e347a1f4f2769451213630d
richals <richalsthomask@gmail.com>
2021-02-19 20:18:52 +0530

    v1

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,6 +2,7 @@ import './design/homepage.css'
 import React,{Component} from 'react';
 import Companyinfo from './companyinfo';
 import axios from 'axios';
+import jfile from './jsonfile';
 
 export default class Homepage extends React.Component{
 
@@ -13,19 +14,7 @@ export default class Homepage extends React.Component{
                
         };
     }
-    handlemouseenter=()=>{
-        
-        document.getElementById("view").style.left = "10%";
-        document.getElementById("menu").style.right = "0%";
-        
-    }
-
-    handlemouseleave=()=>{
-        
-        document.getElementById("view").style.left = "3%";
-        document.getElementById("menu").style.right = "7%";
-        
-    }
+    
 
     handleevent=()=>{
         this.setState({ popup: !this.state.popup });
@@ -34,30 +23,125 @@ export default class Homepage extends React.Component{
     togglepopup(){
         this.setState({popup:!this.state.popup});
     }
-    butto(){
-        axios.post(' https://hoblist.com/movieList', 
-            { category: 'movies', language: 'kannada', genre: 'all', sort: 'voting' }).then(response => alert(response.data));
+   
+    butto() {
+        
+        return jfile.map((value, index) => {
+            
+            
+            return <div Style="height:40%;width:98%;padding:1%;text-align:center">
+                
+                     <div Style="float:left;height:100%;width:10%;">
+
+                    <div Style="background-image:url(download.png);background-size:cover;height:20%;width:39%;left:15%;position:relative;"></div>
+
+                    <h3 Style="right:17%;position:relative;font-size:30px">
+                        
+                        {value.totalVoted}
+                        
+                    </h3>
+
+                    <div Style="background-image:url(download1.jpg);background-size:cover;height:20%;width:39%;left:13%;position:relative;">
+
+                    </div>
+
+                    <h5 Style="right:16%;position:relative;font-size:20px;bottom:5%;">
+                        votes
+                        
+                    </h5>
+
+                     </div>
+                <div  Style="float:left;height:100%;width:15%;">
+                    
+                    <img id="poster" src={value.poster} Style="height:100%;width:100%" ></img>
+
+                     </div>
+
+                <div Style="float:right;height:100%;width:75%;text-align:left;">
+
+                         <a Style="font-style:italic;font-size:30px;left:5%;position:relative">
+                             
+                             {value.title}
+                             
+                            </a>
+                            
+                    <br/><br/>
+
+                    <a Style="font-size:20px;position:relative;left:30px;color:grey;">
+                        Genre&nbsp;:
+                    </a>
+                    
+                    <a Style="font-size:20px;position:relative;left:30px;">
+                        &nbsp;{value.genre}
+                    </a>
+                    
+                    <br/>
+
+                    <a Style="font-size:20px;position:relative;left:30px;color:grey;">
+                        Director&nbsp;:&nbsp;
+                    </a>
+                    
+                    <a Style="font-size:20px;position:relative;left:30px;">
+                        {value.director}&nbsp;,&nbsp;
+                    </a>
+                    
+                    <br/>
+
+                    <a Style="font-size:20px;position:relative;left:30px;color:grey;">
+                        Starring&nbsp;:&nbsp;
+                    </a>
+                    
+                    <a Style="font-size:20px;position:relative;left:30px;">
+                        {value.stars}&nbsp;,&nbsp;
+                    </a>
+                    
+                    <br/>
+
+                    <a Style="font-size:20px;position:relative;left:30px;color:grey;">
+                        Language&nbsp;:&nbsp;
+                    </a>
+                    
+                    <a Style="font-size:20px;position:relative;left:30px;">
+                        {value.language}
+                    </a>
+                    
+                    <br/>
+
+                    <a Style="font-size:20px;position:relative;left:30px;color:blue">
+                        Views&nbsp;:&nbsp;{value.pageViews}&nbsp;|&nbsp;Voted by&nbsp;{value.totalVoted}&nbsp;people
+                    </a>
+                    
+                    <br /><br />
+                    
+                    <a id="button" href={"https://www.youtube.com/results?search_query=trailer+".concat(value.title)} target="_blank">Watch Trailer</a>
+                     </div>
+                  
+                
+                </div>
+            
+        })
+        
     }
 
     render(){
         return(
-            <body Style="height:100vh;width:203vh;overflow-x:hidden;overflow-y:hidden;">
-            <div id="menu" onMouseEnter={()=>{this.handlemouseenter()}} onMouseLeave={()=>{this.handlemouseleave()}} >
-                <a Style="color:white;font-size:20px;">menu</a>
-                <hr/>
-                <div/>
-                <a id="cinfo" onClick={()=>{this.handleevent()}}>Company Info</a>
-            </div>
-            <div id="view">
-                    <div Style="right:75%;top:10%;position:relative">
-
-                    <button  Style="" onClick={()=>{this.butto()}}>click</button>
-                    </div>
-            </div>
-                {
-                    this.state.popup ?
-                        <Companyinfo closepopup={this.togglepopup.bind(this)} /> : null
-                }
+            <body Style="height:100vh;width:100%;">
+                <div Style="width:100%;background-color:black;height:5%;"  >
+                    
+                    <a Style="color:white;font-size:20px;" >menu</a>
+                    <span Style="padding-left:10px;padding-right:10px;width:5px;border-right:5px solid white"></span>
+                    &nbsp;&nbsp;&nbsp;
+                    <a id="cinfo" onClick={() => { this.handleevent() }}>Company Info</a>
+                </div>
+                <div Style="height:95%;width:100%;overflow-y:auto;" id="view">
+                {this.butto()}
+                    {
+                        this.state.popup ?
+                            <Companyinfo closepopup={this.togglepopup.bind(this)} /> : null
+                    }
+                    
+                </div>
+                
             </body>
         );
     }
---
+++ src/pages/jsonfile.js
@@ -0,0 +1,354 @@
+ var jfile=[
+    {
+        "_id": "5d9448106e6974498e97e844",
+        "description": "",
+        "director": [
+            "Prashanth Neel"
+        ],
+        "writers": [
+            "Prashanth Neel"
+        ],
+        "stars": [
+            "Sanjay Dutt,Raveena Tandon,Yash,Srinidhi Shetty"
+        ],
+        "productionCompany": [
+            "Prashanth Neel"
+        ],
+        "pageViews": 141,
+        "upVoted": [],
+        "downVoted": [],
+        "title": "K.G.F: Chapter 2",
+        "language": "Kannada",
+        "runTime": null,
+        "releasedDate": 1601580599,
+        "genre": "action,drama",
+        "voted": [
+            {
+                "upVoted": [
+                    "5fe18f0e2337772a083bf60f"
+                ],
+                "downVoted": [],
+                "_id": "5d9448106e6974498e97e846",
+                "updatedAt": 1569998864,
+                "genre": "action"
+            },
+            {
+                "upVoted": [
+                    "5e1d4475416a070ee7add99c",
+                    "5fe18f0e2337772a083bf60f"
+                ],
+                "downVoted": [],
+                "_id": "5d9448106e6974498e97e845",
+                "updatedAt": 1569998864,
+                "genre": "drama"
+            }
+        ],
+        "poster": "https://s3.ap-south-1.amazonaws.com/hoblist/movies/poster/1569998864699_K.G.F:_Chapter 2.jpg",
+        "totalVoted": 0,
+        "voting": 0
+    },
+    {
+        "_id": "5e7069c83247fc29fe7f55b1",
+        "description": "",
+        "director": [
+            "Rana Sunil Kumar Singh"
+        ],
+        "writers": [
+            ""
+        ],
+        "stars": [
+            "Vaibhavi Joshi,Rajesh Nataranga,Srinivas Prabhu,Arvind Rau"
+        ],
+        "productionCompany": [
+            ""
+        ],
+        "pageViews": 288,
+        "upVoted": [
+            "5e5e1bcdb57a766c57b2c451"
+        ],
+        "downVoted": [],
+        "title": "Www.Meenabazar.,",
+        "language": "Kannada",
+        "runTime": null,
+        "releasedDate": 1584646199,
+        "genre": "drama",
+        "voted": [
+            {
+                "upVoted": [],
+                "downVoted": [],
+                "_id": "5e7069c83247fc29fe7f55b2",
+                "updatedAt": 1584425416,
+                "genre": "drama"
+            }
+        ],
+        "poster": "https://s3.ap-south-1.amazonaws.com/hoblist/movies/poster/1584425416478_Www.Meenabazar.,.jpg",
+        "totalVoted": 1,
+        "voting": 1
+    },
+    {
+        "_id": "5e65cbee6f8b393aca0ca2e4",
+        "description": "",
+        "director": [
+            "Vikram Yoganand"
+        ],
+        "writers": [
+            ""
+        ],
+        "stars": [
+            "Guruprasad,Chandu Gowda,Kailash Pal"
+        ],
+        "productionCompany": [
+            ""
+        ],
+        "pageViews": 322,
+        "upVoted": [
+            "5e782aaf82a1d60c67ecd4a6"
+        ],
+        "downVoted": [],
+        "title": "Kushka",
+        "language": "Kannada",
+        "runTime": 122,
+        "genre": "drama",
+        "voted": [
+            {
+                "upVoted": [],
+                "downVoted": [],
+                "_id": "5e65cbee6f8b393aca0ca2e5",
+                "updatedAt": 1583729646,
+                "genre": "drama"
+            }
+        ],
+        "poster": "https://hoblist.s3.ap-south-1.amazonaws.com/movies/poster/1583729863157_70022372.jpg",
+        "releasedDate": 1584041399,
+        "totalVoted": 1,
+        "voting": 1
+    },
+    {
+        "_id": "5e65d02c6f8b393aca0ca308",
+        "description": "",
+        "director": [
+            "Dore Raj Teja"
+        ],
+        "writers": [
+            ""
+        ],
+        "stars": [
+            "Supreem,Asha,Kavya"
+        ],
+        "productionCompany": [
+            ""
+        ],
+        "pageViews": 329,
+        "upVoted": [
+            "5e782aaf82a1d60c67ecd4a6"
+        ],
+        "downVoted": [],
+        "title": "Ambani Putra",
+        "language": "Kannada",
+        "runTime": null,
+        "genre": "drama",
+        "voted": [
+            {
+                "upVoted": [],
+                "downVoted": [],
+                "_id": "5e65d02c6f8b393aca0ca309",
+                "updatedAt": 1583730732,
+                "genre": "drama"
+            }
+        ],
+        "poster": "https://hoblist.s3.ap-south-1.amazonaws.com/movies/poster/1583730898323_IMG-20200219-WA0140.jpg",
+        "releasedDate": 1584041399,
+        "totalVoted": 1,
+        "voting": 1
+    },
+    {
+        "_id": "5e7059c93247fc29fe7f5580",
+        "description": "",
+        "director": [
+            "Tarun Sudhir"
+        ],
+        "writers": [
+            "Tarun Sudhir"
+        ],
+        "stars": [
+            "Darshan Thoogudeep,Jagapathi Babu,Asha Bhat,Vinod Prabhakar"
+        ],
+        "productionCompany": [
+            "Tarun Sudhir"
+        ],
+        "pageViews": 209,
+        "upVoted": [
+            "5fa16e812337772a083bef69"
+        ],
+        "downVoted": [],
+        "title": "Roberrt",
+        "language": "Kannada",
+        "runTime": null,
+        "releasedDate": 1583782199,
+        "genre": "action",
+        "voted": [
+            {
+                "upVoted": [],
+                "downVoted": [],
+                "_id": "5e7059c93247fc29fe7f5581",
+                "updatedAt": 1584421321,
+                "genre": "action"
+            }
+        ],
+        "poster": "https://s3.ap-south-1.amazonaws.com/hoblist/movies/poster/1584421321076_Roberrt.jpg",
+        "totalVoted": 1,
+        "voting": 1
+    },
+    {
+        "_id": "5e5c913ca587862e1b597c18",
+        "description": "",
+        "director": [
+            "Pramod Chakravarthi"
+        ],
+        "writers": [
+            ""
+        ],
+        "stars": [
+            "Shivarajkumar,Rangayana Raghu"
+        ],
+        "productionCompany": [
+            ""
+        ],
+        "pageViews": 311,
+        "upVoted": [
+            "5e6779066f8b393aca0cbd69"
+        ],
+        "downVoted": [],
+        "title": "Drona",
+        "language": "Kannada",
+        "runTime": null,
+        "genre": "drama",
+        "voted": [
+            {
+                "upVoted": [],
+                "downVoted": [],
+                "_id": "5e5c913ca587862e1b597c19",
+                "updatedAt": 1583124796,
+                "genre": "drama"
+            }
+        ],
+        "poster": "https://hoblist.s3.ap-south-1.amazonaws.com/movies/poster/1583124864967_drona-20190903173036-4083.jpg",
+        "releasedDate": 1583436599,
+        "totalVoted": 1,
+        "voting": 1
+    },
+    {
+        "_id": "5cdea3fcc5cdee1bf917dbc6",
+        "director": [
+            " Radhakrishna Reddy"
+        ],
+        "writers": [
+            ""
+        ],
+        "stars": [
+            "Prakash Raj,Raj B. Shetty,Vasishta N. Simha,Achyuth Kumar"
+        ],
+        "releasedDate": 1582831799,
+        "productionCompany": [
+            ""
+        ],
+        "title": "Mayabazaar 2016",
+        "language": "Kannada",
+        "runTime": null,
+        "genre": "drama",
+        "voted": [
+            {
+                "upVoted": [],
+                "downVoted": [],
+                "_id": "5cdea3fcc5cdee1bf917dbc7",
+                "updatedAt": 1566647798,
+                "genre": "drama"
+            }
+        ],
+        "pageViews": 327,
+        "description": "",
+        "poster": "https://hoblist.s3.ap-south-1.amazonaws.com/movies/poster/1582549586018_mayabazar2016-20200217110337-17046.jpg",
+        "totalVoted": 0,
+        "voting": 0
+    },
+    {
+        "_id": "5e53c9925bf4cf14f67006c0",
+        "description": "",
+        "director": [
+            "Hari Santhosh"
+        ],
+        "writers": [
+            "B.L. Venu"
+        ],
+        "stars": [
+            "Raja Varadhan,Hariprriya,Prabhakar,Sparsha Rekha"
+        ],
+        "productionCompany": [
+            "B.L. Venu"
+        ],
+        "pageViews": 189,
+        "upVoted": [],
+        "downVoted": [],
+        "title": "Bicchugatthi Chapter 1",
+        "language": "Kannada",
+        "runTime": 141,
+        "releasedDate": 1582831799,
+        "genre": "action,thriller",
+        "voted": [
+            {
+                "upVoted": [],
+                "downVoted": [],
+                "_id": "5e53c9925bf4cf14f67006c2",
+                "updatedAt": 1582549394,
+                "genre": "action"
+            },
+            {
+                "upVoted": [],
+                "downVoted": [],
+                "_id": "5e53c9925bf4cf14f67006c1",
+                "updatedAt": 1582549394,
+                "genre": "thriller"
+            }
+        ],
+        "poster": "https://s3.ap-south-1.amazonaws.com/hoblist/movies/poster/1582549394172_Bicchugatthi_Chapter 1.jpg",
+        "totalVoted": 0,
+        "voting": 0
+    },
+    {
+        "_id": "5e4cc4235bf4cf14f67001bc",
+        "description": "",
+        "director": [
+            "Akash Srivatsa"
+        ],
+        "writers": [
+            ""
+        ],
+        "stars": [
+            "Ramesh Aravind,Avinash,Rohith Bhanuprakash,Radhika Narayan"
+        ],
+        "productionCompany": [
+            ""
+        ],
+        "pageViews": 288,
+        "upVoted": [],
+        "downVoted": [],
+        "title": "Shivaji Surathkal",
+        "language": "Kannada",
+        "runTime": null,
+        "releasedDate": 1582226999,
+        "genre": "drama",
+        "voted": [
+            {
+                "upVoted": [],
+                "downVoted": [],
+                "_id": "5e4cc4235bf4cf14f67001bd",
+                "updatedAt": 1582089251,
+                "genre": "drama"
+            }
+        ],
+        "poster": "https://hoblist.s3.ap-south-1.amazonaws.com/movies/poster/1582089322387_74186309.jpeg",
+        "totalVoted": 0,
+        "voting": 0
+    },
+]
+export default jfile;
